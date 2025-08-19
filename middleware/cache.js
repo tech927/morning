0269aadc7f421fca03c4d65cc02c68165b1891e3
tech927 <onnui688@gmail.com>
2025-08-19 18:52:57 -0300

    Create cache.js

---
+++ middleware/cache.js
@@ -0,0 +1,7 @@
+export const noCache = (req, res, next) => {
+  res.setHeader('Surrogate-Control', 'no-store');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+};
