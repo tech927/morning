44a4faddfae4cf27d9a6d60c33ca62691bdbc8ee
tech927 <onnui688@gmail.com>
2025-08-19 17:52:45 -0300

    Create Follow.js

---
+++ models/Follow.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const followSchema = new mongoose.Schema({
+  follower: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  following: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+followSchema.index({ follower: 1, following: 1 }, { unique: true });
+
+export default mongoose.model('Follow', followSchema);
